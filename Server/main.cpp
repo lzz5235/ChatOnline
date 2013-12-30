1115d54e9d4e99322fe798156d180eb80d4894a6
lzz <ldm5235@gmail.com>
2013-12-30 17:33:57 +0800

    add localcodec

--- Server/main.cpp
+++ Server/main.cpp
@@ -3,6 +3,9 @@
 
 int main(int argc, char *argv[])
 {
+
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForLocale(codec);
     QApplication a(argc, argv);
 
     MainWindow w;
